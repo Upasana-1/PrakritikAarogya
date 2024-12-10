97d7b45e438e1994ec180434a9ddb41d5e52bab4
Rajesh-K-C <rajeshkc1111.rk@gmail.com>
2024-12-10 23:59:37 +0545

    create django project, core app, navbar, and setup tailwind css

---
+++ PrakritikAarogya/static/js/navbar.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    document.getElementById("menu-icon").classList.toggle("hidden");
+    document.getElementById("close-icon").classList.toggle("hidden");
+    document.getElementById("mobile-menu").classList.toggle("hidden");
+}
